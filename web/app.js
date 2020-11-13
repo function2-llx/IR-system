2ebddb1e65263760a780f8dfa2714954a5a227e5
function2 <function2@qq.com>
2020-11-13 18:43:35 +0800

    web

---
+++ web/app.js
@@ -0,0 +1,74 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+var bodyParser = require('body-parser')
+
+app.use(express.static(path.join(__dirname, 'static')))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({//处理以form表单的提交
+    extended: true
+}))
+
+const { Client } = require('@elastic/elasticsearch')
+const client = new Client({
+    node: 'http://localhost:9200',
+})
+
+app.post('/search', function(req, res) {
+    let tokens = req.body.tokens;
+    console.log(tokens);
+    mapped_tokens = []
+    tokens.forEach((token_pos) => {
+        token_pos = token_pos.filter(x => x);
+        if (token_pos.length > 0) {
+            mapped_tokens.push(token_pos.join('_'));
+        }
+    })
+    let query = mapped_tokens.join(' ');
+    console.log(query);
+    if (!query) return;
+    client.search({
+        index: 'docs',
+        body: {
+            query: {
+                match: { content: query }
+            },
+            highlight: {
+                pre_tags: ["<b>"], 
+                post_tags: ["</b>"],
+                fields: {
+                    content: {}
+                }
+            }
+        }
+    }, (err, results) => {
+        if (err) {
+            console.log(err);
+            res.json({
+                msg: "出错力",
+            });
+            return;
+        }
+        let hits = results.body.hits.hits;
+        console.log(hits);
+        results = hits.map(hit => {
+            let content = hit.highlight.content[0].split(' ').map(x => {
+                x = x.split('_')[0];
+                if (x.startsWith('<b>')) x += '</b>';
+                return x;
+            }).join('');
+            console.log(content);
+            return {
+                score: hit._score,
+                content: content,
+            }
+        })
+        res.json({
+            results: results,
+        })
+    });
+})
+
+app.listen(8080, () => {
+  console.log('App listening at port 8080')
+})
