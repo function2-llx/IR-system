4e3779e7979ac5b82cbc3e88ec195f98406747e8
function2 <function2@qq.com>
2020-11-14 16:45:00 +0800

    search with constraints

--- web/app.js
+++ web/app.js
@@ -7,32 +7,79 @@ app.use(express.static(path.join(__dirname, 'static')))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({//处理以form表单的提交
     extended: true
-}))
+}));
 
 const { Client } = require('@elastic/elasticsearch')
-const client = new Client({
-    node: 'http://localhost:9200',
-})
+const client = new Client({node: 'http://localhost:9200'})
 
 app.post('/search', function(req, res) {
-    let tokens = req.body.tokens;
-    console.log(tokens);
-    mapped_tokens = []
+    let data = JSON.parse(req.body.data);
+    console.log(data);
+    let tokens = data.tokens;
+    let constraints = data.constraints;
+    mappedTokens = []
     tokens.forEach((token_pos) => {
         token_pos = token_pos.filter(x => x);
-        if (token_pos.length > 0) {
-            mapped_tokens.push(token_pos.join('_'));
-        }
+        mappedTokens.push(token_pos.join('_'));
     })
-    let query = mapped_tokens.join(' ');
-    console.log(query);
-    if (!query) return;
+    let query = mappedTokens.filter(x => x).join(' ');
+    if (!query) {
+        res.json({
+            total: 0,
+            hits: [],
+        });
+        return;
+    }
+
+    console.log('query:', query);
+    let must = [{match: { content: query }}];
+    let must_not = [];
+    let lnear = (token1, token2) => { return {match_phrase: {content: [token1, token2].join(' ')}}; };
+    let rnear = (token1, token2) => { return {match_phrase: {content: [token2, token1].join(' ')}}; };
+    let near = (token1, token2) => {
+        return {
+            bool: {
+                should: [
+                    lnear(token1, token2),
+                    rnear(token1, token2)
+                ]
+            }
+        };
+    };
+    constraints.forEach(([id1, id2, r]) => {
+        let ids = [id1, id2].filter(id => {
+            id = parseInt(id) - 1;
+            if (id < 0 || mappedTokens.length <= id) return false;
+            return mappedTokens[id];
+        });
+        if (ids.length < 2) return;
+        [token1, token2] = ids.map(id => mappedTokens[id - 1]);
+        switch (r) {
+            case '相邻': must.push(near(token1, token2));
+            break;
+            case '不相邻': must_not.push(near(token1, token2));
+            break;
+            case '左邻': must.push(lnear(token1, token2));
+            break;
+            case '不左邻': must_not.push(lnear(token1, token2));
+            break;
+            case '右邻': must.push(rnear(token1, token2));
+            break;
+            case '不右邻': must_not.push(rnear(token1, token2));
+            break;
+        }
+    });
+    console.log('must:', must);
+    console.log('must_not:', must_not);
     client.search({
         index: 'docs',
         body: {
-            from: req.body.from,
+            from: data.from,
             query: {
-                match: { content: query }
+                bool: {
+                    must: must,
+                    must_not: must_not,
+                }
             },
             highlight: {
                 pre_tags: ["<b>"],
@@ -59,13 +106,11 @@ app.post('/search', function(req, res) {
                 if (x.startsWith('<b>')) x += '</b>';
                 return x;
             }).join('');
-            console.log(content);
             return {
                 score: hit._score,
                 content: content,
             }
         })
-        console.log(hits);
         res.json({
             total: total,
             hits: hits,
@@ -73,6 +118,8 @@ app.post('/search', function(req, res) {
     });
 })
 
-app.listen(8080, () => {
-  console.log('App listening at port 8080')
+
+let port = 8080;
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
 })
